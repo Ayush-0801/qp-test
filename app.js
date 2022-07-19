56167b88813419e4f537ea2af134fee845ad619f
Ayush-0801 <ayush19301@iiitd.ac.in>
2022-07-19 15:28:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ const port = process.env.PORT || '5000';
 var spotifyApi = new SpotifyWebApi({
     clientId: 'e5528e5bb8b24755ad89dbc0eae5bea8',
     clientSecret: 'c265137ac990469890c0b7e447d5ca23',
-    redirectUri: 'https://spotifyapi-qp.herokuapp.com:'+port+'/callback'
-
+    // redirectUri: 'https://spotifyapi-qp.herokuapp.com:'+port+'/callback'
+    redirectUri: 'https://www.google.com'
 });
 
 var access_token;
